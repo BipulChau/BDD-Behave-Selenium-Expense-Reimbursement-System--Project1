46c600efa5fb976c56de3348e20f0e9436a26ef8
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-27 13:03:22 -0400

    Input correct username

--- features/steps/loginsteps.py
+++ features/steps/loginsteps.py
@@ -1,22 +1,31 @@
 from behave import *
+import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 
-@given(u'Employee is in the log in page')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given Employee is in the log in page')
+@given('Employee is in the log in page')
+def open_browser(context):
+    context.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    context.driver.get("http://127.0.0.1:5501/index.html")
+    time.sleep(1)
 
 
 @when(u'Input correct username')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When Input correct username')
+def input_username(context):
+    input_correct_username = context.driver.find_element(By.XPATH, "//*[@id='username']")
+    input_correct_username.send_keys('bipul513')
+    time.sleep(3)
+    context.driver.quit()
 
 
 @when(u'correct password')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When correct password')
+def input_correct_password(context):
+    pass
 
 
 @then(u'redirect to logged in page')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then redirect to logged in page')
+    pass
