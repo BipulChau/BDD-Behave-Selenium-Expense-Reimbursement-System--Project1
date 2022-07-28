676a283ea7aa89bf029bc9aebae500387ff95d88
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-28 17:09:19 -0400

    Feature login Scenario correct username and correct password

--- features/steps/loginsteps.py
+++ features/steps/loginsteps.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 def open_browser(context):
     context.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     context.driver.get("http://127.0.0.1:5501/index.html")
-    time.sleep(1)
+    time.sleep(3)
 
 
 @when(u'Input correct username')
@@ -24,15 +24,19 @@ def input_correct_username(context):
 def input_correct_password(context):
     correct_password = context.driver.find_element(By.XPATH, "//*[@id='password']")
     correct_password.send_keys('password')
-    time.sleep(3)
-    context.driver.quit()
+    time.sleep(1)
 
 
 @when('click Login')
 def click_login(context):
-    pass
+    login_button = context.driver.find_element(By.XPATH, "//*[@id='login']")
+    login_button.click()
+    time.sleep(1)
 
 
-@then(u'redirect to logged in page')
-def step_impl(context):
-    pass
+@then('redirect to logged in page')
+def redirect_to_logged_in_page(context):
+    window_after = context.driver.window_handles[0]
+    context.driver.switch_to.window(window_after)
+    time.sleep(1)
+    context.driver.quit()
